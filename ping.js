53c8ee6f71126c5e21ef544b37d31209baac385b
Jay Soffian <jaysoffian@gmail.com>
2023-05-17 15:03:28 -0400

    ping.js: improve CLI ergonomics

--- ping.js
+++ ping.js
@@ -1,10 +1,41 @@
 #!/usr/bin/env node
 "use strict";
 const Jvc = require("./jvc");
-const { argv } = require("node:process");
+const { argv, exit } = require("node:process");
+
+function usage() {
+  console.log("usage: ping.js [-d|--debug] <host>");
+  exit(1);
+}
+
+function parseArgs() {
+  const args = {
+    host: undefined,
+    debug: false,
+  };
+  for (const arg of argv.slice(2)) {
+    if (["--debug", "-d"].includes(arg)) {
+      args.debug = true;
+    } else if (arg.startsWith("-")) {
+      console.log(`bad option: '${arg}'`);
+      usage();
+    } else if (!args.host) {
+      args.host = arg;
+    } else {
+      console.log(`host already specified: '${args.host}'`);
+      usage();
+    }
+  }
+  if (!args.host) {
+    console.log("must specify host");
+    usage();
+  }
+  return args;
+}
 
 async function ping() {
-  const jvc = new Jvc(argv[2]);
+  const args = parseArgs();
+  const jvc = new Jvc(args.host, args.debug ? console.log : () => undefined);
   const power = await jvc.getPower();
   console.log(`Power ${power}`);
   console.log(`Model ${await jvc.getModel()}`);
